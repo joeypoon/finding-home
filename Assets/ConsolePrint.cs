433d13c11f64b571781c75606b2972f10476f812
Joey F. Poon <joey@kaijudev.com>
2017-01-15 19:56:49 -0600

    Basic welcome screen.

--- Assets/ConsolePrint.cs
+++ Assets/ConsolePrint.cs
@@ -6,8 +6,25 @@ public class ConsolePrint : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        float location = 5.0f;
+        float homeLocation = 2.1f;
+        print("Welcome to Finding Home.");
+        print("A game where you need to find your way home.");
+        print("You are " + (location - homeLocation) + " meters from home.");
+
+        if (location > homeLocation)
+        {
+            print("Go back to find home!");
+        }
+        else if (homeLocation < location)
+        {
+            print("Go forward to get home!");
+        }
+        else
+        {
+            print("You're home!");
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {

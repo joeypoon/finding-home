aba33ede29a30da2fccd254f404ccd7d7605174d
Joey F. Poon <joey@kaijudev.com>
2017-01-16 21:56:09 -0600

    Basic vector calculations.

--- Assets/ConsolePrint.cs
+++ Assets/ConsolePrint.cs
@@ -4,37 +4,41 @@ using UnityEngine;
 
 public class ConsolePrint : MonoBehaviour {
 
+    Vector2 location;
+    Vector2 homeLocation;
+    Vector2 pathToHome;
+
 	// Use this for initialization
 	void Start () {
-        float location = 5.0f;
-        float homeLocation = 2.1f;
+        location = new Vector2(3.0f, 2.0f);
+        homeLocation = new Vector2(0, 0);
+        pathToHome = homeLocation - location;
         print("Welcome to Finding Home.");
         print("A game where you need to find your way home.");
-        print("You are " + (location - homeLocation) + " meters from home.");
-
-        if (location > homeLocation)
-        {
-            print("Go back to find home!");
-        }
-        else if (homeLocation < location)
-        {
-            print("Go forward to get home!");
-        }
-        else
-        {
-            print("You're home!");
-        }
+        print("Path to home" + location);
+        print("Distance to home: " + pathToHome.magnitude);
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
-            print("Left arrow pressed");
+            location = new Vector2(location.x - 1, location.y);
         }
         else if (Input.GetKeyUp(KeyCode.RightArrow))
         {
-            print("Right arrow pressed");
+            location = new Vector2(location.x + 1, location.y);
         }
+        else if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            location = new Vector2(location.x, location.y + 1);
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            location = new Vector2(location.x, location.y - 1);
+        }
+        pathToHome = homeLocation - location;
+        print("Path to home" + location);
+        print("Distance to home: " + pathToHome.magnitude);
     }
 }

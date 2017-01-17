261f160894e4ce7c409268c4c83b6435a88280d5
Joey F. Poon <joey@kaijudev.com>
2017-01-16 22:06:15 -0600

    Refactor console prints.

--- Assets/ConsolePrint.cs
+++ Assets/ConsolePrint.cs
@@ -12,11 +12,9 @@ public class ConsolePrint : MonoBehaviour {
 	void Start () {
         location = new Vector2(3.0f, 2.0f);
         homeLocation = new Vector2(0, 0);
-        pathToHome = homeLocation - location;
         print("Welcome to Finding Home.");
         print("A game where you need to find your way home.");
-        print("Path to home" + location);
-        print("Distance to home: " + pathToHome.magnitude);
+        PrintDistance();
     }
 	
 	// Update is called once per frame
@@ -24,21 +22,35 @@ public class ConsolePrint : MonoBehaviour {
         if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
             location = new Vector2(location.x - 1, location.y);
+            PrintDistance();
         }
         else if (Input.GetKeyUp(KeyCode.RightArrow))
         {
             location = new Vector2(location.x + 1, location.y);
+            PrintDistance();
         }
         else if (Input.GetKeyUp(KeyCode.UpArrow))
         {
             location = new Vector2(location.x, location.y + 1);
+            PrintDistance();
         }
         else if (Input.GetKeyUp(KeyCode.DownArrow))
         {
             location = new Vector2(location.x, location.y - 1);
+            PrintDistance();
         }
+    }
+
+    void PrintDistance()
+    {
         pathToHome = homeLocation - location;
-        print("Path to home" + location);
-        print("Distance to home: " + pathToHome.magnitude);
+        if (pathToHome.magnitude == 0)
+        {
+            print("You're home!");
+        }
+        else
+        {
+            print("Distance to home: " + pathToHome.magnitude);
+        }
     }
 }
